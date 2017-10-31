dc6a7f03d3e1319456dd378d8ce518dfbaede381
kys10 <skyisno.1@gmail.com>
2017-10-31 11:06:19 +0800

    Upload collecting daemon and config file

---
+++ run.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import configargparse
+import subprocess
+import json
+from datetime import datetime
+import time
+import sys
+
+def get_gpu_stat(hostname):
+    try:
+        cmd_args = ['ssh', hostname, 'gpustat', '--json']
+        result = subprocess.check_output(cmd_args)
+        return result
+    except subprocess.CalledProcessError as e:
+        return None
+
+if __name__ == '__main__':
+    parser = configargparse.ArgParser(default_config_files=['./config'])
+    parser.add('-c', required=False, is_config_file=True)
+    parser.add('--hosts', action='append')
+    parser.add('--interval', type=int)
+    parser.add('--log_path')
+
+    args = parser.parse_args()
+
+    while True:
+        try:
+            now = datetime.now()
+
+            results = []
+            print(str(now))
+            for hostname in args.hosts:
+                result = get_gpu_stat(hostname)
+                if result is not None:
+                    result = json.loads(result)
+                    results.append(result)
+                    print('get result from {}'.format(hostname))
+
+                else:
+                    print('get error from {}'.format(hostname))
+
+            print '=' * 89
+            output_json = {'time': str(now), 'data': results}
+            with open(args.log_path + '/' + 'all.log', 'a') as f:
+                f.write(json.dumps(output_json) + '\n')
+
+            sys.stdout.flush()
+            time.sleep(args.interval)
+
+        except KeyboardInterrupt:
+            print('-' * 89)
+            print('Exiting from keyboard interrupt')
+            break
+
+
